bb3b551c974af09305887fc4160376ff70bf9958
trreitan <trreitan@gmail.com>
2016-09-15 11:00:45 +0200

    Update http.go

--- http.go
+++ http.go
@@ -11,7 +11,7 @@ func hello(w http.ResponseWriter, r *http.Request) {
 }
 
 func to_roman(n int) string {
-    var decimalValue := [13]int{ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 }
+    decimalValue := [13]int{ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 }
     //var romanNumeral = [ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" ]
     //var romanized = ""
     
